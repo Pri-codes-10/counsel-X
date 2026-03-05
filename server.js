d309fbcc42d438d28dd9a8efc4949882ae5f9c2f
Priyansh Ghosh <priyansh0804@gmail.com>
2026-03-05 08:51:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("CounselX API running");
+});
+
+mongoose.connect(process.env.Mongo_url)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
